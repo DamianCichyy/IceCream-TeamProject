69db2183d0afd24d0830973a3c5bb4381e4dd5e6
Damian Cichy <damiancichy91@gmail.com>
2022-10-17 10:54:21 +0200

    .

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,12 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.openModalBtn.forEach(el => el.addEventListener('click', toggleModal));
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
