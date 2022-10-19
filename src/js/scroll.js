6f26841312c50930b2e9eeefdba5c953c180e2a4
Damian Cichy <damiancichy91@gmail.com>
2022-10-19 07:05:53 +0200

    Updates js functions

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,17 +1,19 @@
+(() => {
+  const links = document.querySelectorAll('.header  ul a, .mobile__container-nav-link');
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-<script type="text/javascript">
-      const links = document.querySelectorAll('.header  ul a');
-      for (const link of links) {
-        link.addEventListener('click', clickHandler);
-      }
-      function clickHandler(e) {
-        e.preventDefault();
-        const href = this.getAttribute('href');
-        const offsetTop = document.querySelector(href).offsetTop;
-        scroll({
-          top: offsetTop,
-          behavior: 'smooth',
-        });
-      }
-    </script>
-   
+  for (const link of links) {
+    link.addEventListener('click', clickHandler);
+  }
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const offsetTop = document.querySelector(href).offsetTop;
+    mobileMenu.classList.contains('is-open') && window.innerWidth < 768 ? closeMenuBtn.click() : '';
+    scroll({
+      top: offsetTop,
+      behavior: 'smooth',
+    });
+  }
+})();
